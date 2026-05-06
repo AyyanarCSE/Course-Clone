cc53dd1db8ae6539de3b763b0aaf9d0fea50a40c
Ayyanar <ayyanarlmc@gmail.com>
2026-05-06 12:46:51 +0530

    Update coursera.js

--- coursera.js
+++ coursera.js
@@ -1,4 +1,3 @@
-// togglePlans.js
 document.addEventListener("DOMContentLoaded", () => {
   const teamBtn = document.getElementById("secondbtn");
   const individualBtn = document.getElementById("firstbtn");
@@ -6,19 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const individualSection = document.getElementById("individualSection");
 
   teamBtn.addEventListener("click", () => {
-    teamSection.style.display = "block";
-    individualSection.style.display = "none";
+    teamSection.classList.remove("hidden");
+    individualSection.classList.add("hidden");
+
     teamBtn.classList.add("bg-blue-600", "text-white");
     teamBtn.classList.remove("bg-white", "text-black");
+
     individualBtn.classList.remove("bg-blue-600", "text-white");
     individualBtn.classList.add("bg-white", "text-black");
   });
 
   individualBtn.addEventListener("click", () => {
-    individualSection.style.display = "block";
-    teamSection.style.display = "none";
+    individualSection.classList.remove("hidden");
+    teamSection.classList.add("hidden");
+
     individualBtn.classList.add("bg-blue-600", "text-white");
     individualBtn.classList.remove("bg-white", "text-black");
+
     teamBtn.classList.remove("bg-blue-600", "text-white");
     teamBtn.classList.add("bg-white", "text-black");
   });
